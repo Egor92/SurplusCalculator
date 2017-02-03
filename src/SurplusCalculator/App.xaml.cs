37fb72a62c97e3e6e175ee13007da934c20dd7df
Egor Novikov <novikov.ea@mail.ru>
2017-02-04 01:59:32 +0300

    UI implemented #3

--- src/SurplusCalculator/App.xaml.cs
+++ src/SurplusCalculator/App.xaml.cs
@@ -1,8 +1,26 @@
 ﻿using System.Windows;
+using SurplusCalculator.Infrastructure;
+using SurplusCalculator.ViewModels;
+using SurplusCalculator.Views;
 
 namespace SurplusCalculator
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var window = new Window
+            {
+                Content = new MainView
+                {
+                    DataContext = new MainViewModel(new FileSelector()),
+                },
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                Width = 600,
+                Height = 500,
+                Title = "Surplus calculator"
+            };
+            window.Show();
+        }
     }
 }
